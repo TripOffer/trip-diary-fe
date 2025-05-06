54bf6de498e73299f980dbbc8ce076af1a52511f
JiMei-University-Zhuang <202241054088@jmu.edu.cn>
2025-05-06 17:44:11 +0800

    :sparkles: 完善个人主页

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import styles from './index.module.scss';
+import ProfileHeader from './ProfileHeader';
+import ProfileContent from './ProfileContent';
 
-const Profile: React.FC = () => <div>我的页面（Profile）</div>;
+const Profile: React.FC = () => {
+  const userData = {
+    name: '用户test',
+    userId: '349595329',
+    avatar: 'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg',
+  };
+
+  const statsData = {
+    followCount: 243,
+    fansCount: 19,
+    likeCount: 7,
+  };
+
+  return (
+    <div className={styles.container}>
+      {/* 顶部个人信息区 */}
+      <ProfileHeader userData={userData} statsData={statsData} />
+
+      {/* 内容区Content */}
+      <ProfileContent />
+    </div>
+  );
+};
 
 export default Profile;

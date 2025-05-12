23a880581e990469a84a3ac2740b248e9d01eebf
MengChen <tatsukimengchen@163.com>
2025-05-12 20:29:29 +0800

    feat: 添加双击返回退出功能，优化 WebView 返回逻辑

--- src-tauri/gen/android/app/src/main/java/com/tripOffer/diary/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/tripOffer/diary/MainActivity.kt
@@ -2,10 +2,15 @@ package com.tripOffer.diary
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.webkit.WebView
+import android.widget.Toast
 
 class MainActivity : TauriActivity() {
   private var webViewRef: WebView? = null
+  private var lastBackPressedTime: Long = 0
+  private val doubleBackPressInterval = 2000L // 2秒内双击返回
 
   @SuppressLint("SetJavaScriptEnabled")
   override fun onWebViewCreate(webView: WebView) {
@@ -27,6 +32,22 @@ class MainActivity : TauriActivity() {
     super.onConfigurationChanged(newConfig)
     setStatusBarByTheme()
     webViewRef?.let { WebViewHelper.updateWebViewBackgroundColor(this, it) }
+  }
+
+  override fun onBackPressed() {
+    webViewRef?.let { webView ->
+      if (webView.canGoBack()) {
+        webView.goBack()
+        return
+      }
+    }
+    val currentTime = System.currentTimeMillis()
+    if (currentTime - lastBackPressedTime < doubleBackPressInterval) {
+      super.onBackPressed()
+    } else {
+      lastBackPressedTime = currentTime
+      Toast.makeText(this, "再按一次返回键退出应用", Toast.LENGTH_SHORT).show()
+    }
   }
 
   private fun setStatusBarByTheme() {
